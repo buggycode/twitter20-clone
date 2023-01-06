3c76565c18031d4eb60bb60dc531a1bd76ca935f
Yoongyoung kim <yoongyoungkim@Yoongyoungs-MacBook-Pro.local>
2023-01-06 17:14:32 +0800

    All

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitter: '#00ADED'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
